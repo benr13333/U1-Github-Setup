a135c66a58f23adb9dc1ff1e4c1f9013ff048215
hanzoshomado <139906045+hanzoshomado@users.noreply.github.com>
2025-09-23 22:57:21 -0400

    Updated UI

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,40 @@
 import java.util.Scanner;
 public class Main {
+
     public static void main(String[] args)
     {
+
+        //Color Codes
+        String GREEN  = "\u001B[32m";
+        String YELLOW = "\u001B[33m";
+        String BLUE   = "\u001B[34m";
+        String CYAN   = "\u001B[36m";
+        String RESET  = "\u001B[0m";
+
+
         Scanner scan = new Scanner(System.in);  // create a Scanner "object"
 
+        //Top Header
+        System.out.println(GREEN + "════════════════════════════════════════════════════════" );
+        System.out.println(GREEN + "║" + CYAN + "                   TIP CALCULATOR                     " + GREEN + "║");
+        System.out.println(GREEN + "║" + CYAN + "!Important! | Do not use symbols when inputting data." + GREEN + " ║");
+        System.out.println("════════════════════════════════════════════════════════");
 
-        System.out.println("!Important! | Do not use symbols when inputting data.");
-        System.out.println("--------------------------------------------------------");
-        System.out.print("Enter your bill($): ");
+        //Inputs
+        System.out.println("\n══════════════════════════════");
+        System.out.print(CYAN + "Enter your bill($): ");
         double value = scan.nextDouble();
         System.out.print("Tip Percent: ");
         int tipPercent = scan.nextInt();
         System.out.print("Number of People: ");
         int numOfPeople = scan.nextInt();
+        System.out.println(GREEN + "══════════════════════════════");
 
 
         //convert our value double to a string in order to check the amnt of digits
         String valueString = Double.toString(value);
         int decimalPointIndex = valueString.indexOf('.');
         String subString =  valueString.substring(decimalPointIndex);
-
-
 
 
         //logic checks
@@ -30,16 +44,16 @@ public class Main {
             double billCost = Math.round((tipAmount + value) * 100.0) / 100.0;
             double tipPerPerson = Math.round((tipAmount / numOfPeople) * 100.0) / 100.0;
             double billPerPerson = Math.round((billCost / numOfPeople) * 100.0) / 100.0;
-            System.out.println("--------------------------------------------------------");
-            System.out.println();
-            System.out.println("Total tip amount: " + tipAmount);
-            System.out.println("--------------------------------------------------------|");
-            System.out.println("Total bill cost: " + billCost);
-            System.out.println("--------------------------------------------------------|");
-            System.out.println("Tip per person: " + tipPerPerson);
-            System.out.println("--------------------------------------------------------|");
-            System.out.println("Total bill per person: " + billPerPerson);
-            System.out.println("--------------------------------------------------------|");
+            System.out.println("\n" + GREEN + "╔══════════════════════════════════════════════════════╗" + RESET);
+            System.out.println(GREEN + "║  " + BLUE + "Total tip amount: " + YELLOW + "$" + tipAmount + RESET);
+            System.out.println(GREEN + "║══════════════════════════════════════════════════════" + RESET);
+            System.out.println(GREEN + "║  " + BLUE + "Total bill cost: " + YELLOW + "$" + billCost + RESET);
+            System.out.println(GREEN + "║══════════════════════════════════════════════════════" + RESET);
+            System.out.println(GREEN + "║  " + BLUE + "Tip per person: " + YELLOW + "$" + tipPerPerson + RESET);
+            System.out.println(GREEN + "║══════════════════════════════════════════════════════" + RESET);
+            System.out.println(GREEN + "║  " + BLUE + "Total bill per person: " + YELLOW + "$" + billPerPerson + RESET);
+            System.out.println(GREEN + "╚══════════════════════════════════════════════════════╝" + RESET);
+
 
 
         }
@@ -51,6 +65,3 @@ public class Main {
 
     }
 }
-
-
-
